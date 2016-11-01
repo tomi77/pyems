43403b964dfbbe5c849942df802b65e2640f90e3
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-11-01 18:33:09 +0100

    new version: 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyems',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/tomi77/pyems',
     license='MIT',
