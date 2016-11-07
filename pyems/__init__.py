a3932f7fd077e22aa361f1696388afbee007e9b0
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-11-07 18:56:10 +0100

    Better error handling for unknown protocol

--- pyems/__init__.py
+++ pyems/__init__.py
@@ -10,7 +10,10 @@ from .utils import expected, get_module_class, EvoStreamException
 class Api(object):
     def __init__(self, uri):
         url = urlparse.urlparse(uri)
-        protocol_class = get_module_class(SCHEMES[url.scheme])
+        try:
+            protocol_class = get_module_class(SCHEMES[url.scheme])
+        except KeyError:
+            raise EvoStreamException('Invalid uri "%s"' % uri)
         self.protocol = protocol_class('%s:%d' % (url.hostname, url.port))
 
     @expected('uri', 'keepAlive', 'localStreamName', 'forceTcp', 'tcUrl',
